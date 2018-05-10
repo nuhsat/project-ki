6bad1682f208d7724503a59388574a2acd083f9e
Harits514 <m.harits7@gmail.com>
2018-05-10 20:43:43 +0700

    Login Register Fixed

--- sign-in/login/core/user.php
+++ sign-in/login/core/user.php
@@ -12,40 +12,35 @@
 //untuk login
 function cek_data($unama, $pass){
 
-  $cipher = new AES(); // could use AES::MODE_CBC
-  // keys are null-padded to the closest valid size
-  // longer than the longest key and it's truncated
+  $cipher = new AES();
   //$cipher->setKeyLength(128);
   $cipher->setKey($pass);
   // the IV defaults to all-NULLs if not explicitly defined
   $cipher->setIV('kE4m4N4n-1nF012m4s1');
 
- session_start();
+ //session_start();
  $_SESSION['unama'] = $unama;
  $_SESSION['pass']= $pass;
 
-  $unama = $cipher->encrypt($unama);
-  $pass = $cipher->encrypt($pass);
-
-
+  $cunama = $cipher->encrypt($unama);
 
   global $link;
   //mencegah sql injection
-  $unama = mysqli_real_escape_string($link, $unama);
+  $cunama = mysqli_real_escape_string($link, $cunama);
   $pass = mysqli_real_escape_string($link, $pass);
 
-  $query = "SELECT id_user,password FROM user WHERE username ='$unama'";
+  $query = "SELECT id_user,password FROM user WHERE username ='$cunama'";
   $result = mysqli_query($link, $query);
   $hash = mysqli_fetch_assoc($result)['password'];
+  $e_pass = $cipher->decrypt($hash);
 
-  if( password_verify($pass, $hash) ){
-    return false;
-  }
-  else{
+  if( $pass == $e_pass) {
     $id_user = mysqli_fetch_assoc($result)['id_user'];
-
     $_SESSION['id_user']=$id_user;
     return true;
+  }
+  else{
+    return false;
   }
 }
 ?>
--- sign-in/signup/signup.php
+++ sign-in/signup/signup.php
@@ -1,6 +1,10 @@
 <?php
-
 require_once "core/init.php";
+require '../../vendor/autoload.php';
+
+use phpseclib\Crypt\AES;
+use phpseclib\Crypt\Random;
+
 
 if( isset($_POST['submit']) ){
   $unama = $_POST['username'];
@@ -10,8 +14,12 @@ if( isset($_POST['submit']) ){
 
   if (!empty(trim($unama)) && !empty(trim($pass)) ){
 
+  $cipher = new AES();
+  $cipher->setKey($pass);
+  $cipher->setIV('kE4m4N4n-1nF012m4s1');
+  $cunama=$cipher->encrypt($unama);
 
-  if(register_cek_nama($unama)){
+  if(register_cek_nama($cunama)){
     //memasukan ke database
         if(register_user($unama, $pass)){
           header('Location: ../../index.php');
